707db6be59886e8f70d720e0b98f22b9de97dd99
anthonyeden <anthonyeden@8b5c5b56-841d-0410-b502-cf68dfdfd60c>
2006-09-16 02:55:39 +0000

    Fixture now supports the List interface.

--- trunk/activerecord/src/com/aetrion/activerecord/fixture/Fixture.java
+++ trunk/activerecord/src/com/aetrion/activerecord/fixture/Fixture.java
@@ -2,17 +2,23 @@ package com.aetrion.activerecord.fixture;
 
 import com.aetrion.activerecord.adapter.Adapter;
 import com.aetrion.activerecord.ActiveRecord;
+import com.aetrion.activerecord.errors.ActiveRecordException;
 import com.aetrion.activesupport.Strings;
 import com.aetrion.activesupport.Classes;
 
 import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * A fixture is used to prepare a database table for testing.
  *
+ * Note: this class is not thread safe
+ *
  * @author Anthony Eden
  */
-public class Fixture {
+public class Fixture implements List {
+
+    private List<Object> fields;
 
     /**
      * Construct a new Fixture
@@ -52,6 +58,115 @@ public class Fixture {
                 ar.includePrimaryKey = false;
             }
         }
+    }
+
+    protected List<Object> getFields() {
+        if (fields == null) {
+            fields = new ArrayList<Object>();
+            for (Field f : getClass().getDeclaredFields()) {
+                try {
+                    f.setAccessible(true);
+                    fields.add(f.get(this));
+                } catch (IllegalAccessException e) {
+                    throw new ActiveRecordException(e);
+                } finally {
+                    f.setAccessible(false);
+                }
+            }
+        }
+        return fields;
+    }
+
+    public int size() {
+        return getFields().size();
+    }
+
+    public boolean isEmpty() {
+        return getFields().isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return getFields().contains(o);
+    }
+
+    public Iterator iterator() {
+        return getFields().iterator();
+    }
+
+    public Object[] toArray() {
+        return getFields().toArray();
+    }
+
+    public boolean add(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean addAll(Collection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean addAll(int i, Collection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Object get(int i) {
+        return getFields().get(i);
+    }
+
+    public Object set(int i, Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void add(int i, Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Object remove(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int indexOf(Object o) {
+        return getFields().indexOf(o);
+    }
+
+    public int lastIndexOf(Object o) {
+        return getFields().lastIndexOf(o);
+    }
+
+    public ListIterator listIterator() {
+        return getFields().listIterator();
+    }
+
+    public ListIterator listIterator(int i) {
+        return getFields().listIterator(i);
+    }
+
+    public List subList(int i, int i1) {
+        return getFields().subList(i, i1);
+    }
+
+    public boolean retainAll(Collection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean removeAll(Collection collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean containsAll(Collection collection) {
+        return getFields().containsAll(collection);
+    }
+
+    public Object[] toArray(Object[] objects) {
+        return getFields().toArray(objects);
     }
 }
 
--- trunk/activerecord/test/src/com/aetrion/activerecord/ActiveRecordsTest.java
+++ trunk/activerecord/test/src/com/aetrion/activerecord/ActiveRecordsTest.java
@@ -4,6 +4,8 @@ import com.aetrion.activerecord.models.Topic;
 import com.aetrion.activerecord.fixture.Topics;
 import junit.framework.TestCase;
 
+import java.util.List;
+
 /**
  * Test the ActiveRecords class.
  *
@@ -17,7 +19,29 @@ public class ActiveRecordsTest extends TestCase {
         topics = new Topics();
     }
 
-    public void testFindOne() {
+    public void testFindFirst() {
+        List<? extends ActiveRecord> records = ActiveRecords.find(FindType.FIRST, Topic.class, null);
+        assertNotNull(records);
+        assertEquals(1, records.size());
+        Topic topic = (Topic) records.get(0);
+        assertEquals(topics.first.getTitle(), topic.getTitle());
+    }
+
+    public void testFindAll() {
+        List<? extends ActiveRecord> records = ActiveRecords.find(FindType.ALL, Topic.class, null);
+        assertNotNull(records);
+        assertEquals(2, records.size());
+        for (int i = 0; i < topics.size() ; i++) {
+            Topic topic = (Topic) topics.get(i);
+            assertEquals(topic.getTitle(), ((Topic) records.get(i)).getTitle());
+            // Right now the assertion below fails because the type coming from the fixture is a Long whereas
+            // the type coming from AR is an int.
+            // TODO: Need to figure out how to deal with this.
+            // assertEquals(topic.getId(), records.get(i).getId());
+        }
+    }
+
+    public void testFindFirstMethod() {
         Topic topic = (Topic) ActiveRecords.findFirst(Topic.class);
         assertNotNull(topic);
         assertEquals(topics.first.getTitle(), topic.getTitle());
